eec811c596c36487d0253dabd32f853ce7f10e2e
abixy <70246562+abixy@users.noreply.github.com>
2023-01-05 13:10:47 -0500

    set back to false

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,6 +3,6 @@ export default {
 	description: `A trip through the mind of an emerging animation visionary.`,
 	url: `https://abixy.com`, // No trailing slash!
 	name: `TheWoodsWork`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
+  enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
