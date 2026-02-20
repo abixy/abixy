605ab99835e1b308d29fe0f4fe6ad20463fa7f0f
abixy <70246562+abixy@users.noreply.github.com>
2026-02-20 12:18:11 -0500

    removed

--- src/config/settings.js
+++
@@ -1,8 +0,0 @@
-export default {
-	title: `The Woods Work | Animation professional portfolio and CV`,
-	description: `A trip through the mind of an emerging animation visionary.`,
-	url: `https://abixy.com`, // No trailing slash!
-	name: `TheWoodsWork`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: false,
-  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
-};
